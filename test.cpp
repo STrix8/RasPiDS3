479a30ffc40859f7389a27ba3a5add9aa9e80ed4
Owl8 <owl8hukurou@gmail.com>
2015-05-22 08:52:29 +0000

    コンストラクタの呼び方直した enumの要素名間違ってたの直した

--- test.cpp
+++ test.cpp
@@ -6,14 +6,14 @@ using namespace DS3;
 
 int main(void) {
 	cout << "Start" << endl;
-	RasPiDS3 Controller;
+	RasPiDS3 Controller();
 	Controller.update();
 	for (int i = 4096; --i; Controller.update()) {
 		for (int j = 0; j < NumButtons; ++j) {
 			if (Controller.button(j))
 				cout << j << endl;
 		}
-		for (int j = 0; j < StickNums; ++j) {
+		for (int j = 0; j < NumSticks; ++j) {
 			cout << Controller.stick(j) << " ";
 		}
 		cout << endl;
